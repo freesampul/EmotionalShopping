22dfd8a5caa1b4f27a50577750b7a29213cf7e22
sqeejee <98850954+sqeejee@users.noreply.github.com>
2025-01-05 16:25:32 -0800

    changed ./models route

--- public/scripts.js
+++ public/scripts.js
@@ -8,12 +8,12 @@ const run = async () => {
 
     // Load Models
     await Promise.all([
-        faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
-        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-        faceapi.nets.ageGenderNet.loadFromUri('/models'),
+        faceapi.nets.ssdMobilenetv1.loadFromUri('./models'),
+        faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+        faceapi.nets.ageGenderNet.loadFromUri('./models'),
     ]);
 
     const canvas = document.getElementById('canvas');
